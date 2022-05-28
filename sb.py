0238c750d0d6ed3b0cb638af1bf5e84ac7c27399
Azera5 <je.lafontaine92@gmail.com>
2022-05-28 19:52:48 +0200

    Update sb.py

--- sb.py
+++ sb.py
@@ -92,6 +92,112 @@ def get_names(pth):
 def get_cfg_names(pth, type):
     return fnmatch.filter(get_names(pth), type+'_cfg_*.txt')
 
+"""
+Installation
+"""
+
+def view_install_specs(name=0):
+    try:
+        if name==0:
+           return shell('spack find --show-full-compiler')
+        else:
+            return shell('spack find --show-full-compiler '+name)  
+    
+    except Exception as exc:     
+        error_log(' {} [Exception]'.format(type(exc).__name__))
+
+#Prüft Installationsausdruck auf grobe Syntaxfehler
+def check_expr_syn(expr):
+    expr_list=['@%','%@','^%','%^','^@','@^']
+    for _ in expr_list:
+        r=expr.find(_)
+        if r != -1:
+            print('Syntaxfehler an Position: '+str(r))
+            return False
+    return True
+
+#Überprüft ob ein einzelner spec (Name + Version) existiert
+def check_spec(name,version=-1):
+    try:        
+        out = shell('spack info '+str(name))
+        #Falls keine Version vorhanden
+        if version == -1:
+            if out.find('Error')== -1:
+                return True 
+        elif out.find(version) != -1:
+            return True         
+        return False
+    
+    except Exception as exc:     
+        error_log(' {} [Exception]'.format(type(exc).__name__))
+
+#Prüft ob Installationsausdruck gültig ist (alle Specs)
+def check_expr(expr):
+    if check_expr_syn(expr):
+        arr = expr.split('^')              
+        for spec in arr:
+            s=spec.split('%')            
+            if s[0][:0]=='@':
+                #print('Name fehlt!')
+                #Package Name fehlt
+                return False
+               
+            s=spec.split('@')
+            if len(s) > 1:                    
+                if not check_spec(s[0],s[1]):                   
+                    #print(s[0]+'@'+s[1]+' existiert nicht')
+                    #Version existiert nicht
+                    return False
+            else:
+                if not check_spec(s[0]):
+                    #Package existiert nicht
+                    #print(s[0]+' existiert nicht')
+                    return False
+       
+        return True
+    #Syntaxfehler im Ausdruck
+    return False
+
+#Entfernt bereits installierte specs aus Installationsausdruck
+def remove_installed_spec(expr):
+    arr=expr.split('^')
+    all_specs=view_install_specs()
+    out=''
+    for _ in arr:        
+        if all_specs.find(_)==-1:
+            out=out+'^'+_
+    if out!='':        
+        return out[1:]
+    else:
+        return out
+    
+#Schreibt Script zum installieren der specs 
+#TODO: Auslagern der Slurmparameter                    
+def install_spec(expr):
+        if check_expr(expr):
+            expr=remove_installed_spec(expr)            
+            if expr != '':
+                if (os.path.isfile('install.sh'))==False: 
+                    shell('touch install.sh')            
+                s='#!/bin/bash\n' \
+                    + '#SBATCH --nodes=1\n' \
+                    +'#SBATCH --ntasks=1\n' \
+                    +'#SBATCH --partition=vl-parcio\n' \
+                    +'#SBATCH --output=install.out\n' \
+                    +'#SBATCH --error=install.err\n' \
+                    +'echo spack install ' \
+                    + expr
+                #Achtung write besser vorher install.sh leeren
+                file_w('install.sh',' ','a')
+                file_w('install.sh',str(s),0)
+                #print(shell('cat install.sh'))
+                #shell('rm install.sh')
+                return 'Installation läuft'
+            else: 
+                return 'Bereits alles installiert!'
+                
+        else:
+            return 'Kann nicht installiert werden!'
 
 
 """
@@ -357,108 +463,7 @@ def osu_menu(back,txt):
         menu('Eingabe ungültig: Bitte eine Ganzzahl, z.B. 1','3')
 """
 
-def view_install_specs(name=0):
-    try:
-        if name==0:
-           return shell('spack find --show-full-compiler')
-        else:
-            return shell('spack find --show-full-compiler '+name)  
-    
-    except Exception as exc:     
-        error_log(' {} [Exception]'.format(type(exc).__name__))
 
-#Prüft Installationsausdruck auf grobe Syntaxfehler
-def check_expr_syn(expr):
-    expr_list=['@%','%@','^%','%^','^@','@^']
-    for _ in expr_list:
-        r=expr.find(_)
-        if r != -1:
-            print('Syntaxfehler an Position: '+str(r))
-            return False
-    return True
-
-#Überprüft ob ein einzelner spec (Name + Version) existiert
-def check_spec(name,version=-1):
-    try:        
-        out = shell('spack info '+str(name))
-        #Falls keine Version vorhanden
-        if version == -1:
-            if out.find('Error')== -1:
-                return True 
-        elif out.find(version) != -1:
-            return True         
-        return False
-    
-    except Exception as exc:     
-        error_log(' {} [Exception]'.format(type(exc).__name__))
-
-#Prüft ob Installationsausdruck gültig ist (alle Specs)
-def check_expr(expr):
-    if check_expr_syn(expr):
-        arr = expr.split('^')              
-        for spec in arr:
-            s=spec.split('%')            
-            if s[0][:0]=='@':
-                #print('Name fehlt!')
-                #Package Name fehlt
-                return False
-               
-            s=spec.split('@')
-            if len(s) > 1:                    
-                if not check_spec(s[0],s[1]):                   
-                    #print(s[0]+'@'+s[1]+' existiert nicht')
-                    #Version existiert nicht
-                    return False
-            else:
-                if not check_spec(s[0]):
-                    #Package existiert nicht
-                    #print(s[0]+' existiert nicht')
-                    return False
-       
-        return True
-    #Syntaxfehler im Ausdruck
-    return False
-
-#Entfernt bereits installierte specs aus Installationsausdruck
-def remove_installed_spec(expr):
-    arr=expr.split('^')
-    all_specs=view_install_specs()
-    out=''
-    for _ in arr:        
-        if all_specs.find(_)==-1:
-            out=out+'^'+_
-    if out!='':        
-        return out[1:]
-    else:
-        return out
-    
-#Schreibt Script zum installieren der specs 
-#TODO: Auslagern der Slurmparameter                    
-def install_spec(expr):
-        if check_expr(expr):
-            expr=remove_installed_spec(expr)            
-            if expr != '':
-                if (os.path.isfile('install.sh'))==False: 
-                    shell('touch install.sh')            
-                s='#!/bin/bash\n' \
-                    + '#SBATCH --nodes=1\n' \
-                    +'#SBATCH --ntasks=1\n' \
-                    +'#SBATCH --partition=vl-parcio\n' \
-                    +'#SBATCH --output=install.out\n' \
-                    +'#SBATCH --error=install.err\n' \
-                    +'echo spack install ' \
-                    + expr
-                #Achtung write besser vorher install.sh leeren
-                file_w('install.sh',' ','a')
-                file_w('install.sh',str(s),0)
-                #print(shell('cat install.sh'))
-                #shell('rm install.sh')
-                return 'Installation läuft'
-            else: 
-                return 'Bereits alles installiert!'
-                
-        else:
-            return 'Kann nicht installiert werden!'
 
 #Startpunkt
 clear()
