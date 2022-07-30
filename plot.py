264a0e0cb7ab0707e7d3f2faec88df1f4bc5dfb7
Azera5 <je.lafontaine92@gmail.com>
2022-07-30 21:24:56 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,6 +5,16 @@ import glob
 import sys
 
 
+############# Hinweis zur Datenstruktur values ##############
+#                                                           #
+#  values =[[metadata][results]]                            #  
+#  [metadata] = [[titel][x-label][y-label]]                 #
+#  [results]  = [[profil_1],[profil_2],...,[profil_n]]      #
+#  [profil_result] = [[x1,...,xn],[y1,...,yn],...,[label]]  #
+#                                                           #
+#############################################################
+
+
 #Der Timestemp muss ohne [] angegeben werden!
 timestemp=str(sys.argv[1])
 bench=str(sys.argv[2])
@@ -18,61 +28,103 @@ end=path.rfind(']')
 path=path[:end+1]
 
 #Auslesen der Results
-def read_values(timestemp,bench):
-    values = [[],[]]
+def read_values(timestemp,bench):    
     profiles = glob.glob('{}/projects/*'.format(loc)+timestemp+'*results*/*.out')
-    #Alle Results eines Timestemps abarbeiten
+    
+    if bench=='osu':
+        return read_osu(profiles)
+    
+    elif bench=='hpl':
+        return read_hpl(profiles)
+        
+    elif bench=='hpcg':
+        return read_hpcg(profiles)
+
+
+def read_osu(profiles):
+    values = [[],[]]
     for name in profiles:
         if os.stat(name).st_size == 0:
             continue
         values[1].append([[],[],[]])
         with open(name,'r') as f:
-            stringlist = f.readlines()             
+            stringlist = f.readlines() 
 
-        #Auslesen OSU (Results und Labels)
-        if bench == 'osu':
-            #Auslesen MPI-Implementierung
-            with open('{}/configs/'.format(loc)+bench+'/'+os.path.basename(name).replace('.out', '.txt'),'r') as f:
-                values[1][len(values[1])-1][2]='{} ({})'.format(f.readlines()[4].split()[0],name[name.rfind('/')+5:-4])
+        #Auslesen MPI-Implementierung
+        with open('{}/configs/'.format(loc)+'osu/'+os.path.basename(name).replace('.out', '.txt'),'r') as f:
+            values[1][len(values[1])-1][2]='{} ({})'.format(f.readlines()[4].split()[0],name[name.rfind('/')+5:-4])
             
-            #Auslesen der Results
-            for line in stringlist:
-                #Abfangen leerer Zeile
-                if len(line) == 0:
-                    continue
+        #Auslesen der Results
+        for line in stringlist:
+            #Abfangen leerer Zeile
+            if len(line) == 0:
+                continue
                 
-                #Titel Auslesen
-                elif line.find('# OSU MPI ')!=-1:
-                    values[0]=[line[line.find('# OSU MPI ')+10:-5]]
+            #Titel Auslesen
+            elif line.find('# OSU MPI ')!=-1:
+                values[0]=[line[line.find('# OSU MPI ')+10:-5]]
                 
-                #Achsen-Label Auslesen                
-                elif line.find('#') != -1:                    
-                    values[0].extend([x.lstrip() for x in line[2:-1].split(' ',1)])                                     
+            #Achsen-Label Auslesen                
+            elif line.find('#') != -1:                    
+                values[0].extend([x.lstrip() for x in line[2:-1].split(' ',1)])                                     
                 
-                #Ergebnisse Auslesen
-                elif line[0].isdigit():                    
-                    val = line.replace(' ',',',1).replace(' ','').split(',')
-                    values[1][len(values[1])-1][0].append(float(val[0]))
-                    values[1][len(values[1])-1][1].append(float(val[1].replace('\n','')))
+            #Ergebnisse Auslesen
+            elif line[0].isdigit():                    
+                val = line.replace(' ',',',1).replace(' ','').split(',')
+                values[1][len(values[1])-1][0].append(float(val[0]))
+                values[1][len(values[1])-1][1].append(float(val[1].replace('\n','')))
+
+    return values
+
+def read_hpl(profiles):
+    values = [[],[]]
+    for name in profiles:
+        if os.stat(name).st_size == 0:
+            continue
+        values[1].append([[],[],[]])
         
-        #Auslesen HPL (Results und Labels)        
-        if bench == 'hpl':
-            #Profilmerkmale (N,P,Q)
-            values[1][len(values[1])-1][2]='N {}\nP{}; Q{} ({})'.format(stringlist[18].split()[2],stringlist[21].split()[2],stringlist[22].split()[2],name[name.rfind('/')+5:-4])                        
+        with open(name,'r') as f:
+            stringlist = f.readlines() 
+        
+        #Profilmerkmale (N,P,Q)
+        values[1][len(values[1])-1][2]='N {}\nP{}; Q{} ({})'.format(stringlist[18].split()[2],stringlist[21].split()[2],stringlist[22].split()[2],name[name.rfind('/')+5:-4])                        
            
-            #Results
-            val=float(stringlist[46].split()[6].replace('.',''))
-            values[1][len(values[1])-1][0].append(float(profiles.index(name)))
-            values[1][len(values[1])-1][1].append(val)
+        #Results
+        val=float(stringlist[46].split()[6].replace('.',''))
+        values[1][len(values[1])-1][0].append(float(profiles.index(name)))
+        values[1][len(values[1])-1][1].append(val)
             
-            #Achsen-Label
-            values[0]=['HPL Benchmark','Gflops','']    
-    
+        #Achsen-Label
+        values[0]=['HPL Benchmark','Gflops','']
+            
+    return values 
+
+def read_hpcg(profiles):
+    values = [[],[]]
+    for name in profiles:
+        if os.stat(name).st_size == 0:
+            continue
+        values[1].append([[],[],[]])
+        
+        with open(name,'r') as f:
+            stringlist = f.readlines() 
+        
+        #Profilmerkmale UND NAME TODO#####
+        values[1][len(values[1])-1][2]='test: {}'.format(name[name.rfind('/')+5:-4])                        
+           
+        #Results
+        val=float(stringlist[118].split('=')[1])
+        values[1][len(values[1])-1][0].append(float(profiles.index(name)))
+        values[1][len(values[1])-1][1].append(val)
+            
+        #Achsen-Label
+        values[0]=['HPCG Benchmark','Gflops','']
+        
     return values
 
 #Plotten der Results
 def run_plot(timestemp,bench):
-    values = read_values(timestemp,bench)    
+    values = read_values(timestemp,bench)
     fig, ax = plt.subplots()    
     labels=['']
     
@@ -101,6 +153,7 @@ def run_plot(timestemp,bench):
     
     plt.savefig(path+'/'+bench+'_plot.png')
 
+
 #Skalieren der Achsen   
 def ax_scale():
     if bench == 'osu':
@@ -108,6 +161,9 @@ def ax_scale():
         plt.yscale('log')
     
     elif bench == 'hpl':
+        plt.xscale('log',base=10)
+        
+    elif bench == 'hpcg':
         plt.xscale('log',base=10)
 
     
