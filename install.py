f4cd8dd72749d1def8532bdaa6a577bb8b81d1d2
Azera5 <je.lafontaine92@gmail.com>
2022-08-03 16:18:48 +0200

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 import re
 
+from sb import shell, check_expr_syn, error_log
+
+
 meta=sys.argv[1].split('#')
 expr=sys.argv[2].split('#')
 err=''
@@ -11,31 +14,37 @@ err=''
 loc=str(os.path.dirname(os.path.abspath(__file__)))
 
 #Shellfunktion aus sb.py (Hauptprogramm)
+"""
 def shell(cmd):
     #Ausgabe soll nicht direkt auf's Terminal
     p = subprocess.run(str(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)   
     #.stdout liefert einen Binärstring desw. die Dekodierung
     return p.stdout.decode('UTF-8')
 
+
+
 #Prüft Installationsausdruck auf grobe Syntaxfehler
 def check_expr_syn(expr):
     expr_list=['@%','%@','^%','%^','^@','@^']
-    """
+   
+   
     Hinweis bzgl. regulärem Ausdruck:
     {min,max} min/max vorkommen der vorrangegangenen Zeichenkette 
     \w Symbolmenge: [a-z, A-Z, 0-9, _]            
     * Vorangegangene Zeichenkette kommt beliebig oft vor (inkl. 0 mal)
     (...) Gruppe -> praktisch eine Zeichenkette
     \. Punktsymbol, da einfach nur . für einzelnes Zeichen steht            
-    """
+  
     if re.search(r'@{1,1}(\w*\.*[%]{0,0}\w*)*@{1,}',expr):
         return False
     for _ in expr_list:
         r=expr.find(_)        
         if r != -1:
-            os.system('echo Syntaxfehler an Position: {} >> {}/install.err'.format(str(r),loc))
+            sb.error_log('echo Syntaxfehler an Position: {} >> {}/install.err'.format(str(r),loc),locals())
+            sb.err
             return False
     return True
+"""
 
 #Überprüft ob ein einzelner spec (Name + Version) existiert
 def check_spec(name,version=-1):       
@@ -51,7 +60,7 @@ def check_spec(name,version=-1):
 
 #Prüft ob Installationsausdruck gültig ist (alle Specs)
 def check_expr(expr):
-    if check_expr_syn(expr):
+    if check_expr_syn(expr)==True:
         arr = expr.split('^')        
         for spec in arr:
             s=spec.split('%')            
@@ -66,13 +75,13 @@ def check_expr(expr):
                 _=_.split('@')                
                 if len(_) > 1:
                     if not check_spec(_[0],_[1]):                   
-                        #error_log(_[0]+'@'+_[1]+' existiert nicht')
+                        error_log(_[0]+'@'+_[1]+' existiert nicht',locals())
                         #Version existiert nicht
                         return False
                 else:
                     if not check_spec(_[0]):
                         #Package existiert nicht
-                        #error_log(_[0]+' existiert nicht')
+                        error_log(_[0]+' existiert nicht',locals())
                         return False
        
         return True
@@ -87,8 +96,10 @@ def install_spec(expr):
     task=meta[2]
     cpus=meta[3]
     #Check ob angegebene Partition existiert
-    if shell('sinfo -h -p '+partition).find(partition)==-1:
-        return os.system('echo Partition: {} existiert nicht >> {}/install.err'.format(str(partition),loc))         
+    if shell('sinfo -h -p '+partition).find(partition)==-1:        
+        error_log('Partition: {} existiert nicht'.format(str(partition)),locals())
+        #os.system('echo Partition: {} existiert nicht >> {}/install.err'.format(str(partition),loc),locals())
+        return 
         
     slurm=''
     specs=''  
@@ -112,11 +123,12 @@ def install_spec(expr):
         
         #Dokumentieren des Fehlers
         else:
-            os.system('echo {} existiert nicht >> {}/install.err'.format(str(e),loc))
-            
+            #os.system('echo {} existiert nicht >> {}/install.err'.format(str(e),loc))
+            error_log('{} does not exist'.format(str(e),loc),locals())
     
     if len(specs)==0:
-        return os.system('echo bereits alles installiert >> {}/install.err'.format(str(e),loc))                
+        error_log('everything already installed',locals())
+        #return os.system('echo bereits alles installiert >> {}/install.err'.format(str(e),loc))                
     else:        
         return str(slurm+specs)
 
